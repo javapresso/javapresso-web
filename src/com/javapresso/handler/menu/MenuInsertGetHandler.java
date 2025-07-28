ff754469a51f38941c1b66766cc0a19926a2f030
kyeongjun ko <rhrudwnsgjgo@gmail.com>
2025-07-28 23:42:30 +0900

    fix: solved all conflict

--- src/com/javapresso/handler/menu/MenuInsertGetHandler.java
+++ src/com/javapresso/handler/menu/MenuInsertGetHandler.java
@@ -1,21 +1,13 @@
 package com.javapresso.handler.menu;
 
-import java.util.ArrayList;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.javapresso.dao.MenuDao;
 import com.javapresso.handler.CommandHandler;
 
 public class MenuInsertGetHandler implements CommandHandler {
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) {
-		MenuDao menuDao = new MenuDao();
-		ArrayList<String> categories = menuDao.getParentCategories();
-
-		request.setAttribute("parentMenus", categories);
-
 		return "menu/insert.jsp";
 	}
 }
