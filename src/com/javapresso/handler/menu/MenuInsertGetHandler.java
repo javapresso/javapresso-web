e5dee9165e293e4c0c9fb5343cf77e8a49a4a5f6
kyeongjun ko <rhrudwnsgjgo@gmail.com>
2025-07-29 09:54:06 +0900

    feat: add menu insert form category

--- src/com/javapresso/handler/menu/MenuInsertGetHandler.java
+++ src/com/javapresso/handler/menu/MenuInsertGetHandler.java
@@ -1,13 +1,25 @@
 package com.javapresso.handler.menu;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javapresso.dao.MenuDao;
 import com.javapresso.handler.CommandHandler;
 
 public class MenuInsertGetHandler implements CommandHandler {
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) {
+		MenuDao menuDao = new MenuDao();
+		ArrayList<String> categories = menuDao.getParentCategories();
+
+		request.setAttribute("parentMenus", categories);
+		
+		System.out.println("categories " + categories);
+	    for(String category : categories) {
+	        System.out.println("category: " + category + " (class: " + category.getClass().getName() + ")");
+	    }
 		return "menu/insert.jsp";
 	}
 }
